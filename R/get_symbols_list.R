2fcacd9b927706e628f721b83ec01c669bb0244b
aljrico <aljrico@gmail.com>
2019-12-31 19:32:54 +0000

    get_symbols_list

---
+++ R/get_symbols_list.R
@@ -0,0 +1,24 @@
+#' Functions to call and retrieve financial data from remote databases, by using company tickers (symbols). It uses publicly available API to download all data.
+#'
+#' @return Returns an array.
+#'
+#' @author Alejandro Jiménez Rico \email{aljrico@@gmail.com}, \href{https://aljrico.github.io}{Personal Website}
+#'
+#'
+#' @examples
+#'
+#' symbols <- get_symbols_list()
+#' @rdname get_symbols_list
+#' @export
+#'
+#'
+#'
+get_symbols_list <- function(){
+  url <- glue::glue("https://financialmodelingprep.com/api/v3/company/stock/list")
+  url %>%
+    httr::GET() %>%
+    httr::content() %>%
+    .$symbolsList %>%
+    sapply(., FUN = function(x) x$symbol)
+}
+
